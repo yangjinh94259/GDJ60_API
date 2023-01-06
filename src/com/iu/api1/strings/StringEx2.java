c9b7565cf76fc461eccf80bc83c902af6ca0e2b3
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-06 10:45:25 +0900

    StringEx2 Daeki code

--- src/com/iu/api1/strings/StringEx2.java
+++ src/com/iu/api1/strings/StringEx2.java
@@ -5,22 +5,36 @@ import java.util.Scanner;
 public class StringEx2 {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
+		
 		Scanner sc = new Scanner(System.in);
 		String [] names = {"jpg", "jpeg", "gif", "png"};
 		System.out.println("파일명 입력");
 		String fileName = sc.next();
+		boolean flag = true;
 		
-//		for(int i=0; i < names.length; i++) {
-//			String result = fileName.substring(fileName.indexOf(i));
-//			if(result == names) {
-//				System.out.println(names);
-//			}
-//			else {
-//				System.out.println("없음");
-//				break;
-//			}
-//		}
+		//확장자 분리 작업
+		int idx = fileName.lastIndexOf(".");
+		String result = fileName.substring(idx + 1);
+		System.out.println(result);
 		
+		//확장자가 이미지인지 검증
+		for(int i=0; i < names.length; i++) {
+			if(result.equals(names[i])) {		//문자끼리 비교할땐 .equals
+				System.out.println("정상 파일");
+				i = names.length;
+				//flag = false;
+				flag =! flag;
+				break;
+			}
+			
+		}
+		
+		if(flag) {
+			System.out.println("비정상 파일");
+		}
+		
+		System.out.println("Finish");
 	}
 
 }
