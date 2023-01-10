9ac2894d6e098cc40213baf01cce630624efcb92
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-10 09:24:15 +0900

    CollectionStudy

---
+++ src/com/iu/api2/collections/ex1/Ex1Main.java
@@ -0,0 +1,10 @@
+package com.iu.api2.collections.ex1;
+
+public class Ex1Main {
+
+	public static void main(String[] args) {
+		//학생은 이름 번호 국어 영어 수학 총점 평균으로 이루어짐
+		//1. 학생 정보 초기화	2. 학생 정보 전체 조회	3. 학생 정보 검색 조회(이름)	4. 학생 정보 추가	5. 학생 정보 삭제(이름)	6. 프로그램 종료 
+		
+	}
+}
