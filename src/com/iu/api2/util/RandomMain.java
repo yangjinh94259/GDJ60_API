685f10392a4c0afb24ff318a3091d6c8e2495c1b
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-11 09:27:31 +0900

    RandomMainEx Lotto 23/01/11

--- src/com/iu/api2/util/RandomMain.java
+++ src/com/iu/api2/util/RandomMain.java
@@ -10,47 +10,27 @@ public class RandomMain {
 		Random random = new Random();
 		
 		//1000원 로또 구매 1-45 번호6개를 랜덤하게 추출
-//		System.out.println("자동 로또 구매 : 1000원");
-//		int num1 = sc.nextInt();
-//		
-//		int Lotto [] = new int[6];
-//		
-//		System.out.println("구매 완료");
-//		System.out.println("Lotto 랜덤 숫자 ");
-//		
-//		for(int i=0; i < Lotto.length; i++) {
-//			Lotto[i] = (int)(Math.random()*45)+1;
-//			
-//			for(int j=0; j < i; j++) {
-//				if(Lotto[i] == Lotto[j]) {
-//					i--;
-//					break;
-//				}
-//			}
-//		}
-//		
-//		for(int i=0; i < Lotto.length; i++) {
-//			System.out.print(Lotto[i] + "번" + "\t");
-//		}
-	
-		//1. 배열
+		System.out.println("자동 로또 구매 : 1000원");
+		int num1 = sc.nextInt();
 		
+		int Lotto [] = new int[6];
 		
-		int [] nums = new int [6];
+		System.out.println("구매 완료");
+		System.out.println("Lotto 랜덤 숫자 ");
 		
-		for(int i=0; i < nums.length; i++) {
-			
-			nums[i] = random.nextInt(45)+1;
-			
-		}
-		
-		for(int i=0; i < nums.length; i++) {
-			
-			System.out.println(nums[i]);
+		for(int i=0; i < Lotto.length; i++) {
+			Lotto[i] = (int)(Math.random()*45)+1;
 			
 			for(int j=0; j < i; j++) {
-				
+				if(Lotto[i] == Lotto[j]) {
+					i--;
+					break;
+				}
 			}
+		}
+		
+		for(int i=0; i < Lotto.length; i++) {
+			System.out.print(Lotto[i] + "번" + "\t");
 		}
 	
 	}
