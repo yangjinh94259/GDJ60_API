e37822b83e995ce3b5dadb16c8519356470ff25e
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-11 09:49:07 +0900

    RandomMainEx Lotto 23/01/11

--- src/com/iu/api2/util/RandomMain.java
+++ src/com/iu/api2/util/RandomMain.java
@@ -1,6 +1,7 @@
 package com.iu.api2.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,16 +10,32 @@ public class RandomMain {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random();
+		//1000원 로또 구매 1-45 번호 6개를 랜덤하게 추출
 		
-		
+		//1. 배열
 		int [] nums = new int [6];
 		ArrayList<Integer> ar = new ArrayList<>();
 		
 		while(ar.size() < 6) {
 			
-			ar.add(random.nextInt(45)+1);
+			int n = random.nextInt(45)+1;
+			boolean check = true;
 			
+			for(int i=0; i < ar.size(); i++) {
+				if(n != ar.get(i)) {
+					check = false;
+					break;
+				}
+			}
 			
+			if(check) {
+				ar.add(n);
+				
+		}
+			
+	}
+		for(int i=0; i < nums.length; i++) {
+			System.out.println(nums[i]);
 		}
 		
 //		for(int i=0; i < nums.length; i++) {
@@ -30,6 +47,14 @@ public class RandomMain {
 //				}
 //			}
 //		}
+		
+		//2.set
+		HashSet<Integer> hs = new HashSet<>();
+		
+		while(hs.size() < 6) {
+			hs.add(random.nextInt(45)+1);
+		}
+		System.out.println(hs);
 
 	}
 
