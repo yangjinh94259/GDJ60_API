54e62293a806d717d69806798e9ab35322200a08
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-11 13:48:16 +0900

    Calendar 23/01/11

--- src/com/iu/api3/exception/ExceptionMain1.java
+++ src/com/iu/api3/exception/ExceptionMain1.java
@@ -1,10 +1,21 @@
 package com.iu.api3.exception;
 
+import java.util.Random;
+
 public class ExceptionMain1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String num = null;
+		Random random = new Random();
+		int n = random.nextInt(2);
+		if(n == 0) {
+			num = "test";
+		}
+		System.out.println(3 / n);
+		
+		System.out.println(num.charAt(0));
+		
+		System.out.println("종료합니다");
 	}
 
 }
