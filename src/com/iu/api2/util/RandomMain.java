0d7d3ff0df3bbb6225e5ee292a2a47c5f7aaed6b
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-11 09:20:34 +0900

    RandomMainEx Lotto 23/01/11

--- src/com/iu/api2/util/RandomMain.java
+++ src/com/iu/api2/util/RandomMain.java
@@ -1,9 +1,32 @@
 package com.iu.api2.util;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RandomMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Random 
+		
+		
+		int [] nums = new int [6];
+		
+		for(int i=0; i < nums.length; i++) {
+			
+			nums[i] = random.nextInt(45)+1;
+			
+		}
+		
+		for(int i=0; i < nums.length; i++) {
+			
+			System.out.println(nums[i]);
+			
+			for(int j=0; j < i; j++) {
+				
+			}
+		}
+	
 
 	}
 
